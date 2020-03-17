c1a7933586d9e4545f9eafff5e8cf8c0c014d0e0
Sergio DC <sgdc377@gmail.com>
2020-03-17 16:58:58 -0600

    Actulización de script sql

--- db_scripts/twitter.sql
+++ db_scripts/twitter.sql
@@ -1,15 +1,15 @@
 create database twitter;
-use database twitter;
+use twitter;
 
 CREATE TABLE usuario(
-id_usuario INTEGER NOT NULL AUTOINCREMENT,
+id_usuario INTEGER NOT NULL AUTO_INCREMENT,
 nombre VARCHAR(50) NOT NULL UNIQUE,
-pass VARCHAR(50) NOT NULL
-PRIMARY KEY(id_usuario)
+pass VARCHAR(50) NOT NULL,
+PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE comentario(
-id_comentario INTEGER NOT NULL AUTOINCREMENT,
+id_comentario INTEGER NOT NULL AUTO_INCREMENT,
 id_usuario INTEGER  NOT NULL,
 fecha_publicacion DATE NOT NULL,
 contenido VARCHAR(150),
