8e71f1ac31f18ba0f92190e656ef16d66ccdf62e
SergioGeorge <a01365942@itesm.mx>
2020-03-24 16:09:46 -0600

    Se agregaron 2 pruebas para las Publicaciones

--- src/main/java/com/itesm/webapps/primer_parcial/action/PublicacionesAction.java
+++ src/main/java/com/itesm/webapps/primer_parcial/action/PublicacionesAction.java
@@ -18,7 +18,37 @@ public class PublicacionesAction extends ActionSupport{
 			return ERROR;
 		else
 			return SUCCESS;
-	}	
+	}
+	/*La siguiente función permite crear un nevo contenido o crear un réplica a otro usuario*/
+	public String crearNuevoContenido() {
+		boolean seRegistro;
+		
+		if(comentarioBean.getId_respuesta_a() == -1)//Nuevo Contenido			
+			seRegistro = StrutsJsonDAO.insertarComentario(comentarioBean.getId_usuario(), comentarioBean.getContenido(), null);
+		else//Réplica
+			seRegistro = StrutsJsonDAO.insertarComentario(comentarioBean.getId_usuario(), comentarioBean.getContenido(), comentarioBean.getId_respuesta_a());	
+		
+		if(seRegistro)
+			return SUCCESS;
+		else
+			return ERROR;	
+	}
+	/*El siguiente método permite al usuario modificar un comentario siempre y cuando el lo haya creado
+	 * Ojo: la fecha se actualiza autómaticamente cuando actualizamos el comentario
+	 * */
+	public String modificar_contenido() {
+		int noCommentModificado = 0;
+		try {
+			noCommentModificado = StrutsJsonDAO.modificarComentario(comentarioBean.getId_comentario(), comentarioBean.getId_usuario(), comentarioBean.getContenido());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(noCommentModificado);
+		if(noCommentModificado == 0)
+			return ERROR;
+		else
+			return SUCCESS;
+	}
 	
 	public Comentario getComentarioBean() {
 		return comentarioBean;
@@ -26,13 +56,10 @@ public class PublicacionesAction extends ActionSupport{
 	public void setComentarioBean(Comentario comentarioBean) {
 		this.comentarioBean = comentarioBean;
 	}
-
 	public List<ComentarioUsuario> getLista_comentario_usuario() {
 		return lista_comentario_usuario;
 	}
-
 	public void setLista_comentario_usuario(List<ComentarioUsuario> lista_comentario_usuario) {
 		this.lista_comentario_usuario = lista_comentario_usuario;
 	}
-	
 }
--- src/test/java/action/PublicacionesTest.java
+++ src/test/java/action/PublicacionesTest.java
@@ -34,5 +34,53 @@ public class PublicacionesTest extends StrutsTestCase{
 		assertEquals("El método 'mostrarPublicaciones()' debio retornar " + 
 		ActionSupport.SUCCESS + " pero no lo hizo", ActionSupport.SUCCESS, result);
 	}
+	
+	@Test
+	public void testCrearNuevoContenido() throws Exception{
+		StrutsJsonDAO.dropTable("comentario");
+		StrutsJsonDAO.dropTable("usuario");
+		StrutsJsonDAO.create_table("usuario");
+		StrutsJsonDAO.populate_table("usuario");
+		StrutsJsonDAO.create_table("comentario");
+		StrutsJsonDAO.populate_table("comentario");
+		//Al ponerlo como -1 estamos simulando una publicación nueva, es decir que no
+		//es una réplica
+		String id_respuesta_a = "-1";
+		request.setParameter("comentarioBean.id_usuario", "4");
+		request.setParameter("comentarioBean.contenido", "Soy un contenido de testing");
+		request.setParameter("comentarioBean.id_respuesta_a", id_respuesta_a);
+		
+		ActionProxy actionProxy = getActionProxy("/crear_nuevo_contenido.action");
+		PublicacionesAction action = (PublicacionesAction) actionProxy.getAction();
+		
+		assertNotNull("El objecto 'action' es nulo pero no debería serlo", action);
+		assertNotNull("El objecto 'actionProxy' es nulo pero no debería serlo", actionProxy);
+		String result = actionProxy.execute();
+		assertEquals("El método 'crearNuevoContenido()' debio retornar " + 
+		ActionSupport.SUCCESS + " pero no lo hizo", ActionSupport.SUCCESS, result);
+	}
+	
+	@Test
+	public void testModificarPublicacion() throws Exception{
+		StrutsJsonDAO.dropTable("comentario");
+		StrutsJsonDAO.dropTable("usuario");
+		StrutsJsonDAO.create_table("usuario");
+		StrutsJsonDAO.populate_table("usuario");
+		StrutsJsonDAO.create_table("comentario");
+		StrutsJsonDAO.populate_table("comentario");
+		
+		request.setParameter("comentarioBean.id_comentario", "7");
+		request.setParameter("comentarioBean.id_usuario", "3");
+		request.setParameter("comentarioBean.contenido", "Soy una actulización del comentario anterior");
+	
+		ActionProxy actionProxy = getActionProxy("/modificar_contenido.action");
+		PublicacionesAction action = (PublicacionesAction) actionProxy.getAction();
+		
+		assertNotNull("El objecto 'action' es nulo pero no debería serlo", action);
+		String result = actionProxy.execute();
+		
+		assertEquals("El método 'modificarPublicacion()' debio retornar " + 
+		ActionSupport.SUCCESS + " pero no lo hizo", ActionSupport.SUCCESS, result);
+	}
 
 }
